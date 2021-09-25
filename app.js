27a4ced28c9b174dd1a793281731da408f9c0509
Nadine Coelho <nadinecoelho9@gmail.com>
2021-09-25 07:30:32 -0300

    ❗ add a message to error

--- app.js
+++ app.js
@@ -1,30 +1,80 @@
-/*
-  - Construa uma aplicação de conversão de moedas. O HTML e CSS são os que você
-    está vendo no browser;
-  - Você poderá modificar a marcação e estilos da aplicação depois. No momento, 
-    concentre-se em executar o que descreverei abaixo;
-    - Quando a página for carregada: 
-      - Popule os <select> com tags <option> que contém as moedas que podem ser
-        convertidas. "BRL" para real brasileiro, "EUR" para euro, "USD" para 
-        dollar dos Estados Unidos, etc.
-      - O option selecionado por padrão no 1º <select> deve ser "USD" e o option
-        no 2º <select> deve ser "BRL";
-      - O parágrafo com data-js="converted-value" deve exibir o resultado da 
-        conversão de 1 USD para 1 BRL;
-      - Quando um novo número for inserido no input com 
-        data-js="currency-one-times", o parágrafo do item acima deve atualizar 
-        seu valor;
-      - O parágrafo com data-js="conversion-precision" deve conter a conversão 
-        apenas x1. Exemplo: 1 USD = 5.0615 BRL;
-      - O conteúdo do parágrafo do item acima deve ser atualizado à cada 
-        mudança nos selects;
-      - O conteúdo do parágrafo data-js="converted-value" deve ser atualizado à
-        cada mudança nos selects e/ou no input com data-js="currency-one-times";
-      - Para que o valor contido no parágrafo do item acima não tenha mais de 
-        dois dígitos após o ponto, você pode usar o método toFixed: 
-        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-    - Para obter as moedas com os valores já convertidos, use a Exchange rate 
-      API: https://www.exchangerate-api.com/;
-      - Para obter a key e fazer requests, você terá que fazer login e escolher
-        o plano free. Seus dados de cartão de crédito não serão solicitados.
-*/
+const currencyOneEl = document.querySelector('[data-js="currency-one"]');
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]');
+const currenciesEl = document.querySelector('[data-js="currencies-container"]');
+
+const url = `https://v6.exchangerate-api.com/v6/627f79b9feadd1ee5cf4531d/latest/kSD`;
+
+// Try e catch é uma cláusula que vai tentar executar um código try {},
+// se o código que ela tentar executar lançar algum erro, executará o outro código catch(err){} e o erro que foi foi lançado não vai travar o restante do código da aplicação.
+// Fetch retorna uma promise.
+// Só pode lançar um erro quando tiver dentro de um try catch, porque se um erro for lançado sem um catch , esse erro vai travar o código da aplicação.
+// Quando um erro é lançado no try, o objeto de erro é recebido no err do catch e pode ser tratado no catch e o erro que foi lançado no try não vai travar o restante do código
+const getErrorMessage = (errorType) =>
+  ({
+    "unsupported-code": "A moeda não existe em nosso banco de dados",
+    "malformed-request":
+      "O endpoint do seu request precisa seguir a estrutura a seguir: https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD",
+    "invalid-key": "A chave de API não é valida.",
+    "quota-reached":
+      "Sua conta alcançou o limite de requests permitido em seu plano atual.",
+    "inactive-account": "Seu email não foi verificado.",
+  }[errorType] || "Não foi possivel obter as informações.");
+
+const fetchExchangeRate = async () => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(
+        "Sua conexão falhou. Não foi possivel obter as informações"
+      );
+    }
+
+    const exchangeRateData = await response.json();
+
+    if (exchangeRateData.result === "error") {
+      throw new Error(getErrorMessage(exchangeRateData["error-type"]));
+    }
+  } catch (err) {
+    const div = document.createElement("div");
+    const button = document.createElement("button");
+
+    div.textContent = err.message;
+    div.classList.add(
+      "alert",
+      "alert-warning",
+      "alert-dismissible",
+      "fade",
+      "show"
+    );
+    div.setAttribute("role", "alert");
+    button.classList.add("btn-close");
+
+    button.setAttribute("type", "button");
+    button.setAttribute("aria-label", "Close");
+
+    button.addEventListener("click", () => {
+      div.remove();
+    });
+
+    div.appendChild(button);
+    currenciesEl.insertAdjacentElement("afterend", div);
+
+    /*
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  Mensagem do erro
+  <button type="button" class="btn-close"  aria-label="Close"></button>
+</div>
+    
+    */
+  }
+};
+
+fetchExchangeRate();
+
+const option = `<option>oi</option>`;
+
+currencyOneEl.innerHTML = option;
+currencyTwoEl.innerHTML = option;
+
+console.log(currencyOneEl, currencyTwoEl);
